ca8b7ea69d9e7d2d2978066664e5f70d66fcaeb7
Nazwadi <nazwadi@pm.me>
2022-05-01 23:15:18 -0400

    Add table orm mapping for "buyer, faction_base_data, faction_list, faction_list_mod, faction_values" in buyers.py and factions.py for issue #1

---
+++ buyers.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column
+from sqlalchemy.dialects import mysql
+from sqlalchemy.orm import registry
+
+mapper_registry = registry()
+
+
+@mapper_registry.mapped
+class Buyer:
+    """
+    EQEMU Docs URL: https://docs.eqemu.io/schema/buyers/buyer/#schema
+    """
+    __tablename__ = "buyer"
+    charid = Column(mysql.INTEGER(display_width=11), nullable=False, primary_key=True, default=0)
+    buyslot = Column(mysql.INTEGER(display_width=11), nullable=False, primary_key=True, default=0)
+    itemid = Column(mysql.INTEGER(display_width=11), nullable=False, default=0)
+    itemname = Column(mysql.VARCHAR(65), nullable=False)
+    quantity = Column(mysql.INTEGER(display_width=11), nullable=False, default=0)
+    price = Column(mysql.INTEGER(display_width=11), nullable=False, default=0)
---
+++ factions.py
@@ -0,0 +1,56 @@
+from sqlalchemy import Column
+from sqlalchemy.dialects import mysql
+from sqlalchemy.orm import registry
+
+mapper_registry = registry()
+
+
+@mapper_registry.mapped
+class FactionBaseData:
+    """
+    EQEMU Docs URL: https://docs.eqemu.io/schema/factions/faction_base_data/#schema
+    """
+    __tablename__ = "faction_base_data"
+    client_faction_id = Column(mysql.SMALLINT(display_width=6), nullable=False, primary_key=True, default=None)
+    min = Column(mysql.SMALLINT(display_width=6), nullable=True, default=-2000)
+    max = Column(mysql.SMALLINT(display_width=6), nullable=True, default=2000)
+    unk_hero1 = Column(mysql.SMALLINT(display_width=6), nullable=True, default=None)
+    unk_hero2 = Column(mysql.SMALLINT(display_width=6), nullable=True, default=None)
+    unk_hero3 = Column(mysql.SMALLINT(display_width=6), nullable=True, default=None)
+
+
+@mapper_registry.mapped
+class FactionList:
+    """
+    EQEMU Docs URL: https://docs.eqemu.io/schema/factions/faction_list/#schema
+    """
+    __tablename__ = "faction_list"
+    id = Column(mysql.INTEGER(display_width=11), nullable=False, primary_key=True, default=None)
+    name = Column(mysql.VARCHAR(50), nullable=False)
+    base = Column(mysql.SMALLINT(display_width=6), nullable=False, default=0)
+
+
+@mapper_registry.mapped
+class FactionListMod:
+    """
+    EQEMU Docs URL: https://docs.eqemu.io/schema/factions/faction_list_mod/#schema
+    """
+    __tablename__ = "faction_list_mod"
+    id = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False,
+                primary_key=True, default=None, autoincrement="auto")
+    faction_id = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False,
+                        primary_key=True, unique=False, default=None)
+    mod = Column(mysql.SMALLINT(display_width=6), nullable=False, default=None)
+    mod_name = Column(mysql.VARCHAR(16), nullable=False, default=None)
+
+
+@mapper_registry.mapped
+class FactionValues:
+    """
+    EQEMU Docs URL: https://docs.eqemu.io/schema/factions/faction_values/#schema
+    """
+    __tablename__ = "faction_values"
+    char_id = Column(mysql.INTEGER(display_width=4), nullable=False, primary_key=True, default=0)
+    faction_id = Column(mysql.INTEGER(display_width=4), nullable=False, primary_key=True, default=0)
+    current_value = Column(mysql.SMALLINT(display_width=6), nullable=False, default=0)
+    temp = Column(mysql.TINYINT(display_width=3), nullable=False, default=0)
