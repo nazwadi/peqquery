f7531705e3d91d5a7b44b21f1dd324468620323e
Nazwadi <nazwadi@pm.me>
2022-05-08 21:42:51 -0400

    Add tributes.py

---
+++ database/tributes.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column
+from sqlalchemy.dialects import mysql
+from sqlalchemy.orm import registry, relationship
+
+mapper_registry = registry()
+
+
+@mapper_registry.mapped
+class Tributes:
+    """
+    EQEMU Docs URL: https://docs.eqemu.io/schema/tributes/tributes/
+    """
+    __tablename__ = "tributes"
+    id = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False, primary_key=True, default=0)
+    unknown = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False, default=0)
+    name = Column(mysql.VARCHAR(255), nullable=False)
+    descr = Column(mysql.MEDIUMTEXT, nullable=False, default=None)
+    isguild = Column(mysql.TINYINT(display_width=4), nullable=False, primary_key=True, default=0)  # 0 = False, 1 = True
+
+
+@mapper_registry.mapped
+class TributeLevels:
+    """
+    EQEMU Docs URL: https://docs.eqemu.io/schema/tributes/tribute_levels/
+    """
+    __tablename__ = "tribute_levels"
+    tribute_id = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False, primary_key=True, default=0)
+    level = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False, primary_key=True, default=0)
+    cost = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False, default=0)
+    item_id = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False, default=0)
