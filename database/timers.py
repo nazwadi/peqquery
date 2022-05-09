a44d902cdf7a3fd9c6f92d9152bbfad356804dce
Nazwadi <nazwadi@pm.me>
2022-05-08 21:23:33 -0400

    Add timers.py

---
+++ database/timers.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column
+from sqlalchemy.dialects import mysql
+from sqlalchemy.orm import registry
+
+mapper_registry = registry()
+
+
+@mapper_registry.mapped
+class Timers:
+    """
+    EQEMU Docs URL: https://docs.eqemu.io/schema/timers/timers/
+    """
+    __tablename__ = "timers"
+    char_id = Column(mysql.INTEGER(display_width=11), nullable=False, primary_key=True, default=0)
+    type = Column(mysql.MEDIUMINT(display_width=8, unsigned=True), nullable=False, primary_key=True, default=0)
+    start = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False, default=0)
+    duration = Column(mysql.INTEGER(display_width=10, unsigned=True), nullable=False, default=0)
+    enable = Column(mysql.TINYINT(display_width=4), nullable=False, default=0)
