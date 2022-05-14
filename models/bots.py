67ebcfd9db2657cbc185af28e5e53c13cbc33e4b
Nazwadi <nazwadi@pm.me>
2022-05-14 18:34:05 -0400

    Move bots.py to models/

--- models/bots.py
+++ models/bots.py
@@ -11,6 +11,7 @@ class BotBuffs:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_buffs/
     """
     __tablename__ = "bot_buffs"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -19,6 +20,7 @@ class BotData:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_data/
     """
     __tablename__ = "bot_data"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -27,6 +29,7 @@ class BotGroups:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_groups/
     """
     __tablename__ = "bot_groups"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -35,6 +38,7 @@ class BotGroupMembers:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_group_members/
     """
     __tablename__ = "bot_group_members"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -43,6 +47,7 @@ class BotHealRotations:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_heal_rotations/
     """
     __tablename__ = "bot_heal_rotations"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -51,6 +56,7 @@ class BotHealRotationMembers:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_heal_rotation_members/
     """
     __tablename__ = "bot_heal_rotation_members"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -59,6 +65,7 @@ class BotHealRotationTargets:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_heal_rotation_targets/
     """
     __tablename__ = "bot_heal_rotation_targets"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -67,6 +74,7 @@ class BotInspectMessages:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_inspect_messages/
     """
     __tablename__ = "bot_inspect_messages"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -75,6 +83,7 @@ class BotInspectMessages:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_inventories/
     """
     __tablename__ = "bot_inventories"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -83,6 +92,7 @@ class BotOwnerOptions:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_owner_options/
     """
     __tablename__ = "bot_owner_options"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -91,6 +101,7 @@ class BotPets:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_pets/
     """
     __tablename__ = "bot_pets"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -99,6 +110,7 @@ class BotPetBuffs:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_pet_buffs/
     """
     __tablename__ = "bot_pet_buffs"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -107,6 +119,7 @@ class BotPetInventories:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_pet_inventories/
     """
     __tablename__ = "bot_pet_inventories"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -115,6 +128,7 @@ class BotSpellsEntries:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_spells_entries/
     """
     __tablename__ = "bot_spells_entries"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -123,6 +137,7 @@ class BotSpellCastingChances:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_spell_casting_chances/
     """
     __tablename__ = "bot_spell_casting_chances"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -131,6 +146,7 @@ class BotStances:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_stances/
     """
     __tablename__ = "bot_stances"
+    # TODO
 
 
 @mapper_registry.mapped
@@ -139,3 +155,4 @@ class BotTimers:
     EQEMU Docs URL: https://docs.eqemu.io/schema/bots/bot_timers/
     """
     __tablename__ = "bot_timers"
+    # TODO
